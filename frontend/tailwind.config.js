f341d542ffd8ce3835da1dd6ef62bd6a8370a625
SlonZerion <nikitarubtsov42@gmail.com>
2024-10-19 20:49:05 +0300

    .

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,16 @@ export default {
   ],
   theme: {
     extend: {},
+    "animation": {
+      "text-gradient": "text-gradient 1.5s linear infinite"
+    },
+    "keyframes": {
+      "text-gradient": {
+        "to": {
+          "backgroundPosition": "200% center"
+        }
+      }
+    }
   },
   plugins: [],
 }
